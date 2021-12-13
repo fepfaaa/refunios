6b9561ba8b1a84786d8ac9d97386a7a9db681e11
fepfaaa <93022799+fepfaaa@users.noreply.github.com>
2021-12-13 12:42:16 +0700

    Update and rename README.md to refunios.lua

---
+++ refunios.lua
@@ -0,0 +1,6 @@
+1 เพิ่มการชำระเงินเป็นวอเล็ท
+2  ไปซื้อเเพ็คของในเกม
+3 เข้า https://idmsa.apple.com/IDMSWebAuth/signin?appIdKey=20379f32034f8867d352666ff2904d2152d5ff6843ee2db5ab5df863c14b1aef&path=%2F__logged_in%2Freportaproblem.apple.com&authResult=FAILED
+4 ล็อกอิน
+5 เลือก ขอเงินคืน / ฉันไม่ได้ตั้งใจซื้อสินค้านี้
+6 รอเงินเข้า
